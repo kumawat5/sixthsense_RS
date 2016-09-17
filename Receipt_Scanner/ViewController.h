dedf166a8da0d8369f57e2fb100ae0280458b569
ankur kumawat <SixthSenseIT@ankurs-mac.local>
2016-09-17 12:14:42 +0530

    Initial Commit

---
+++ Receipt_Scanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipt_Scanner
+//
+//  Created by ankur kumawat on 17/09/16.
+//  Copyright © 2016 sixthsense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
